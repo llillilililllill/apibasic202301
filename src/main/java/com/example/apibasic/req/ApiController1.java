4d12d2fb6618bb853d5c4776410c0a65cb8894c6
llillilililllill <skenals12@gmail.com>
2023-01-13 11:27:55 +0900

    # 요청 파라미터, 요청 바디 읽기

--- src/main/java/com/example/apibasic/req/ApiController1.java
+++ src/main/java/com/example/apibasic/req/ApiController1.java
@@ -1,13 +1,12 @@
 package com.example.apibasic.req;
 
-import jdk.jfr.Frequency;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
-import java.net.http.HttpHeaders;
 import java.util.Enumeration;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
@@ -21,43 +20,41 @@ public class ApiController1 {
     // 요청을 받아서 처리할 메서드
     @RequestMapping(value = "/hello", method = {GET, POST})
     public String hello(HttpServletRequest request) {
-        log.info("hello~~~ spring! - {}", request.getMethod());
-        log.warn("warn 로그");
-        log.trace("trace 로그");
-        log.debug("debug 로그");
-        log.error("error 로그");
-
+        log.trace("trace 로그~~~~~");
+        log.debug("debug 로그~~~~~");
+        log.info("/say/hello~~~ spring! - {}", request.getMethod());
+        log.warn("warn 로그~~~~~");
+        log.error("error 로그~~~~~");
         return "";
     }
 
-    // GET 요청만 따로 처리하겠다
+    // GET요청만 따로 처리하겠다
 //    @RequestMapping(value = "/greet", method = GET)
     @GetMapping("/greet")
     public String greet() {
-        log.info("/greet GET 요청");
+        log.info("/say/greet GET 요청!!");
         return "";
     }
 
     @PostMapping("/greet")
     public String greet2() {
-        log.info("/greet POST 요청");
+        log.info("/say/greet POST 요청!!");
         return "";
     }
 
-    // 요철 헤더 읽기
+    // 요청 헤더 읽기
     @GetMapping("/header")
-    public String header(HttpServletRequest header) {
+    public String header(HttpServletRequest request) {
 
         String host = request.getHeader("Host");
         String accept = request.getHeader("Accept");
-        String pet = request.getHeader("Pet");
-
-        log.info("========= header info =========");
+        String pet = request.getHeader("pet");
+        log.info("====== header info ======");
         log.info("# host : {}", host);
         log.info("# accept : {}", accept);
         log.info("# pet : {}", pet);
 
-
         return "";
     }
+
 }
